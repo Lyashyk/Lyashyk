44b67c81be3c06e813b17b88040a5ca2ebd1fa76
Aleksndr Illyashenko <alex1002x@gmail.com>
2020-03-26 21:41:53 +0200

    v 2

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("v", 1);
+console.log("v", 2);
+
+let vh = window.innerHeight * 0.01;
+
+document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+window.addEventListener("resize", () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+});
